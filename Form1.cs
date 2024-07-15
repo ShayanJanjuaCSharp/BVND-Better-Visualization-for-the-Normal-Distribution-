43dcea5271dece231d6125307e4dc65830e93e72
Shayan I Janjua <shayanjanjua.londonl@gmail.com>
2024-07-15 21:03:11 +0100

    Initial Design for the Main Menu

--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,37 @@ namespace BVND
         public Form1()
         {
             InitializeComponent();
+            BackColor = Color.SkyBlue;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
